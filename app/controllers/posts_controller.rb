6cf5d679f42f973c795041b5c5de5356ac26f113
Jaison Robson Gusava <jaison.gusava@gett.com.br>
2018-07-24 12:02:05 -0300

    feat(common): primeiro commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,24 @@
+class PostsController < ApplicationController
+    def index
+        @content_first = 'Este e o texto do primeiro comentario'
+        @content_second = 'Este e o texto do segundo comentario'
+    end
+
+    def create
+    end
+
+    def new
+    end
+
+    def edit
+    end
+
+    def show
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+end
