61970ab3eec5d4bd761ed26f1d993fd0f952c27f
Jaison Robson Gusava <jaison.gusava@gett.com.br>
2018-07-25 17:49:14 -0300

    refact(common): iniciado integracao layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'pages/about', :to => 'pages#about'
+  get 'pages/contact', :to => 'pages#contact'
+  get 'pages/resources', :to => 'pages#resources'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   #get 'home/index'
